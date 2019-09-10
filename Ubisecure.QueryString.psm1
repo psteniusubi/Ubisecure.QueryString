bcbd67d6a2db2ceb21cba02e4b2bb1c7d9d7b381
Petteri Stenius <petteri.stenius@ubisecure.com>
2019-09-10 15:08:45 +0300

    version 1.1 formatting

--- Ubisecure.QueryString.psm1
+++ Ubisecure.QueryString.psm1
@@ -1,21 +1,35 @@
 
 function New-QueryString {
-[CmdletBinding()]
-param()
-    [PSCustomObject] @{
-        "PSTypeName" = "QueryString";
-        "Value" = [System.Collections.Specialized.NameValueCollection]::new();
+    [CmdletBinding()]
+    param()
+    Process {
+        [PSCustomObject] @{
+            "PSTypeName" = "QueryString";
+            "Value" = [System.Collections.Specialized.NameValueCollection]::new();
+        }
     }
 }
 
 function Add-QueryString {
-[CmdletBinding()]
-param(
-[parameter(Mandatory=$true,Position=0,ParameterSetName="NameValue")] [string] $Name,
-[parameter(Mandatory=$true,Position=1,ParameterSetName="NameValue")] [AllowEmptyString()] [string] $Value,
-[parameter(Mandatory=$true,Position=0,ParameterSetName="Hashtable")] [hashtable] $Values,
-[parameter(Mandatory=$true,Position=2,ValueFromPipeline=$true)] [PSTypeName("QueryString")] $QueryString 
-)
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true,Position=0,ParameterSetName="NameValue")] 
+        [string] 
+        $Name,
+
+        [parameter(Mandatory=$true,Position=1,ParameterSetName="NameValue")] 
+        [AllowEmptyString()] 
+        [string] 
+        $Value,
+    
+        [parameter(Mandatory=$true,Position=0,ParameterSetName="Hashtable")] 
+        [hashtable] 
+        $Values,
+    
+        [parameter(Mandatory=$true,Position=2,ValueFromPipeline=$true)] 
+        [PSTypeName("QueryString")] 
+        $QueryString 
+    )
     Begin {
         $out = New-QueryString
         switch ($PsCmdlet.ParameterSetName) {
@@ -42,11 +56,16 @@ param(
 }
 
 function Select-QueryString {
-[CmdletBinding()]
-param(
-[parameter(Mandatory=$true,Position=0)] [string[]] $Name,
-[parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)] [PSTypeName("QueryString")] $QueryString = (New-QueryString)
-)
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true,Position=0)] 
+        [string[]] 
+        $Name,
+
+        [parameter(Mandatory=$true,Position=1,ValueFromPipeline=$true)] 
+        [PSTypeName("QueryString")] 
+        $QueryString = (New-QueryString)
+    )
     Process {
         $Name | % {
             $QueryString.Value.GetValues($_)
@@ -55,34 +74,47 @@ param(
 }
 
 function ConvertTo-QueryString {
-[CmdletBinding()]
-param(
-[parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)] [PSTypeName("QueryString")] $QueryString
-)
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,ParameterSetName="QueryString")] 
+        [PSTypeName("QueryString")] 
+        $QueryString,
+
+        [parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true,ParameterSetName="Hashtable")] 
+        [hashtable] 
+        $InputObject
+    )
     Begin {
-        $out = New-QueryString
+        $out = @()
     }
     Process {
-        $out.Value.Add($QueryString.Value)
+        if($QueryString) {
+            $out += $QueryString.Value.AllKeys | % { 
+			    $key = $_
+			    $value = $QueryString.Value[$key]
+                [System.Net.WebUtility]::UrlEncode($key),[System.Net.WebUtility]::UrlEncode($value) -join "=" 
+            }
+        } elseif($InputObject) {
+            $out += $InputObject.Keys | % { 
+			    $key = $_
+			    $value = $InputObject[$key]
+                [System.Net.WebUtility]::UrlEncode($key),[System.Net.WebUtility]::UrlEncode($value) -join "=" 
+            }
+        }
     }
     End {
-        ($out.Value.AllKeys | % {
-            $key = $_
-            $out.Value.GetValues($key) | % {
-                @(
-                    [System.Net.WebUtility]::UrlEncode($key),
-                    [System.Net.WebUtility]::UrlEncode($_)
-                ) -join "="
-            }
-        }) -join "&"
+        $out -join "&"
     }
 }
 
 function ConvertFrom-QueryString {
-[CmdletBinding()]
-param(
-[parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)] [AllowEmptyString()] [string] $Value
-)
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true,Position=0,ValueFromPipeline=$true)] 
+        [AllowEmptyString()] 
+        [string] 
+        $Value
+    )
     Begin {
         Add-Type -AssemblyName "System.Web" -ErrorAction Stop
         $out = New-QueryString
